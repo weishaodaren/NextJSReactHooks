8dd81fcab88032e202eeac2c3b6b2085148745ec
weishaodaren <zhaojiawei@realwaychina.com>
2019-10-31 16:04:50 +0800

    添加babel-plugin-import 配置.babelrc

--- pages/header.js
+++ pages/header.js
@@ -1,6 +1,7 @@
 // import Head from 'next/head'
 // import MyHeader from '../components/myHeader'
 import dynamic from 'next/dynamic'
+import {Button} from 'antd'
 
 const MyHeader = dynamic(import('../components/myHeader'))
 function Header(){
@@ -12,6 +13,7 @@ function Header(){
                 <meta charSet='utf-8'/>
             </Head> */}
             <h1>Happy Coding</h1>
+            <Button>aslkdlas</Button>
         </div>
     )
 }
